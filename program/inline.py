9b1692ea3a996d12478ea87d6003af6d46de88ce
Tharuk Renuja <90763454+TharukRenuja@users.noreply.github.com>
2021-12-16 19:37:33 +0530

    Update inline.py

--- program/inline.py
+++ program/inline.py
@@ -31,7 +31,7 @@ async def inline(client: Client, query: InlineQuery):
                         result["duration"], result["viewCount"]["short"]
                     ),
                     input_message_content=InputTextMessageContent(
-                        "🔗 https://www.youtube.com/watch?v={}".format(result["id"])
+                        "/vplay https://www.youtube.com/watch?v={}".format(result["id"])
                     ),
                     thumb_url=result["thumbnails"][0]["url"],
                 )
